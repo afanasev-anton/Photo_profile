c61b870cb301a9f4b5633c8d50737fe0476eac29
Anton Afanasev <afanasev.anton@icloud.com>
2020-03-02 10:43:02 +0100

    add project files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
+import { AboutPageComponent } from './about-page/about-page.component';
+import { ContactPageComponent } from './contact-page/contact-page.component';
+import { GalleryPageComponent } from './gallery-page/gallery-page.component';
+import { ServicePageComponent } from './service-page/service-page.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+{path:"", component: HomePageComponent},
+{path:"about", component: AboutPageComponent},
+{path:"contact", component: ContactPageComponent},
+{path:"gallery", component: GalleryPageComponent},
+{path:"service", component: ServicePageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { AboutPageComponent } from './about-page/about-page.component';
+import { ContactPageComponent } from './contact-page/contact-page.component';
+import { GalleryPageComponent } from './gallery-page/gallery-page.component';
+import { ServicePageComponent } from './service-page/service-page.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    AboutPageComponent,
+    ContactPageComponent,
+    GalleryPageComponent,
+    ServicePageComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
