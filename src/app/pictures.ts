ad325f67bb0e245022d163f28e16a96acf52d39f
ArijanMA <arijan.sulimani@gmail.com>
2020-03-03 10:46:11 +0100

    added img infos

--- src/app/pictures.ts
+++ src/app/pictures.ts
@@ -8,18 +8,145 @@ pictures.push(
 	tags:"mountains/snow/forest",
 	cat:"nature"},
 
+	{pic:"../../assets/img/mountain2.jpg",
+	takenDate:"2017-04-26",
+	location:"Swiss/CantonGlarus",
+	tags:"mountains/forest",
+	cat:"nature"},
+
+	{pic:"../../assets/img/mountain3.jpg",
+	takenDate:"2010-05-16",
+	location:"Italy/Verona",
+	tags:"mountains/greenforest",
+	cat:"nature"},
+
+	{pic:"../../assets/img/mountain4.jpg",
+	takenDate:"2018-06-16",
+	location:"NorthMazedonia/CrnaGoraMountain",
+	tags:"mountains/sun/forest",
+	cat:"nature"},
+
+	{pic:"../../assets/img/mountain5.jpg",
+	takenDate:"2009-03-16",
+	location:"Albania/AlbanianMountains",
+	tags:"mountains/sun/forest",
+	cat:"nature"},
+
+	{pic:"../../assets/img/mountain6.jpg",
+	takenDate:"2012-07-01",
+	location:"Kosovo/KosovoForest",
+	tags:"mountains/smallriver/forest",
+	cat:"nature"},
+
+	{pic:"../../assets/img/mountain7.jpg",
+	takenDate:"2011-05-26",
+	location:"Sweden/SwedishMountains",
+	tags:"mountains/snow/sun/forest",
+	cat:"nature"},
+
+	{pic:"../../assets/img/mountain8.jpg",
+	takenDate:"2008-02-16",
+	location:"China/ChineseMountains",
+	tags:"mountains/sun/greenforest",
+	cat:"nature"},
+
 	//example for people
 	{pic:"../../assets/img/baby1.jpg",
 	takenDate:"2015-03-22",
+	location:"South Africa",
+	tags:"Son and Mum",
+	cat:"photo"},
+
+	{pic:"../../assets/img/baby2.jpg",
+	takenDate:"2012-04-20",
+	location:"China",
+	tags:"Twins",
+	cat:"portrait"},
+
+	{pic:"../../assets/img/baby3.jpg",
+	takenDate:"2005-07-13",
+	location:"Spain",
+	tags:"Baby",
+	cat:"portrait"},
+
+	{pic:"../../assets/img/baby4.jpg",
+	takenDate:"2010-07-19",
+	location:"USA",
+	tags:"CurlyHairGirl",
+	cat:"portrait"},
+
+	{pic:"../../assets/img/baby5.jpg",
+	takenDate:"2009-02-12",
+	location:"Austria",
+	tags:"youngProgrammer",
+	cat:"CodeFactory"},
+
+	{pic:"../../assets/img/baby6.jpg",
+	takenDate:"2018-09-22",
+	location:"Tailand",
+	tags:"Baby",
+	cat:"portrait"},
+
+	{pic:"../../assets/img/baby7.jpg",
+	takenDate:"2017-06-08",
+	location:"Turkey",
+	tags:"Baby",
+	cat:"portrait"},
+
+	{pic:"../../assets/img/baby8.jpg",
+	takenDate:"2019-03-03",
 	location:"",
-	tags:"",
+	tags:"Russia",
 	cat:"portrait"},
 
 	//example for city
 	{pic:"../../assets/img/city1.jpg",
 	takenDate:"2017-07-07",
-	location:"",
-	tags:"",
+	location:"USA/SanFransisco",
+	tags:"people/cars/buildings",
 	cat:"city"},
+
+	{pic:"../../assets/img/city2.jpg",
+	takenDate:"2010-12-12",
+	location:"Italy/Venezia",
+	tags:"water/buildings/boat",
+	cat:"city"},
+
+	{pic:"../../assets/img/city3.jpg",
+	takenDate:"2014-04-07",
+	location:"NewYork/USA",
+	tags:"people/cars/buildings",
+	cat:"city"},
+
+	{pic:"../../assets/img/city4.jpg",
+	takenDate:"2018-06-19",
+	location:"Greece/Santorini",
+	tags:"sea/sun/summer/houses",
+	cat:"city"},
+
+	{pic:"../../assets/img/city5.jpg",
+	takenDate:"2017-03-13",
+	location:"England/London",
+	tags:"redTelephoneBooth",
+	cat:"city"},
+
+	{pic:"../../assets/img/city6.jpg",
+	takenDate:"2017-07-07",
+	location:"Japan",
+	tags:"buildings/nightTime",
+	cat:"city"},
+
+	{pic:"../../assets/img/city7.jpg",
+	takenDate:"2017-07-07",
+	location:"UAE/Dubai",
+	tags:"Aerial View of City Buildings",
+	cat:"city"},
+
+	{pic:"../../assets/img/city8.jpg",
+	takenDate:"2017-04-15",
+	location:"England/London",
+	tags:"silverCar/buildings",
+	cat:"city"},
+
 
 );
