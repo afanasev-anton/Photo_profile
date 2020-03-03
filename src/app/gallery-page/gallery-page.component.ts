fba5b5cdd921885259eaca52777c7a820be97846
Anton Afanasev <afanasev.anton@icloud.com>
2020-03-03 15:48:38 +0100

    add category filters

--- src/app/gallery-page/gallery-page.component.ts
+++ src/app/gallery-page/gallery-page.component.ts
@@ -12,20 +12,50 @@ export class GalleryPageComponent implements OnInit {
 	locations = [];
 
   constructor() {
-  for (var i = 0; i < this.listOfPictures.length; i++) {
-      if (!this.types.includes(this.listOfPictures[i].cat)) {
-        this.types.push(this.listOfPictures[i].cat);
-      }
-  }
+    for (var i = 0; i < this.listOfPictures.length; i++) {
+        if (!this.types.includes(this.listOfPictures[i].cat)) {
+          this.types.push(this.listOfPictures[i].cat);
+        }
+    }
 
-  for (var i = 0; i < this.listOfPictures.length; i++) {
-      if (!this.types.includes(this.listOfPictures[i].location)) {
-        this.types.push(this.listOfPictures[i].location);
-      }
+    for (var i = 0; i < this.listOfPictures.length; i++) {
+        if (!this.types.includes(this.listOfPictures[i].location)) {
+          this.types.push(this.listOfPictures[i].location);
+        }
+    }
   }
-}
 
   ngOnInit(): void {
   }
 
+  showList(str){
+    if (str == "") {
+      this.listOfPictures = pictures;
+    } else {
+      this.listOfPictures = [];
+      switch (str) {
+        case "Nature":
+          for (var i = 0; i < pictures.length; i++) {
+            if (pictures[i].cat == "Nature") {
+              this.listOfPictures.push(pictures[i]);
+            }
+          }
+          break;
+        case "City":
+          for (var i = 0; i < pictures.length; i++) {
+            if (pictures[i].cat == "City") {
+              this.listOfPictures.push(pictures[i]);
+            }
+          }
+          break;
+        case "Portrait":
+          for (var i = 0; i < pictures.length; i++) {
+            if (pictures[i].cat == "Portrait") {
+              this.listOfPictures.push(pictures[i]);
+            }
+          }
+          break;
+      }
+    }
+  }
 }
